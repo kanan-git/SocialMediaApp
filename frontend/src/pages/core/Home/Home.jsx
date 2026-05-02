e6ed571eec4da5c03295279b294471f6f6a78209
kanan-git <kanan.bakhshaliyev.1997@gmail.com>
2026-05-03 01:08:37 +0400

    regular backup deploy

--- frontend/src/pages/core/Home/Home.jsx
+++ frontend/src/pages/core/Home/Home.jsx
@@ -123,20 +123,104 @@ function Home() {
             </header>
 
             <main className={styles.main}>
-                <aside className={styles.navbar}>
-                    home
-                    explore
-
-                    profile
-                    messages
-                    notifications
-                    settings
-
-                    about
-                    contact
-
-                    create post
+                {/* admin navbar left */}
+                <aside className={styles.adminnav}>
+                    <ul>
+                        <li>
+                            <Link to="/admin/#">
+                                <i className={icons.dashboard}></i>
+                                <span>dashboard</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/#">
+                                <i className={icons.profile}></i>
+                                <span>notifications</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/#">
+                                <i className={icons.people}></i>
+                                <span>users</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/#">
+                                <i className={icons.posts}></i>
+                                <span>posts</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/admin/#">
+                                <i className={icons.comments}></i>
+                                <span>comments</span>
+                            </Link>
+                        </li>
+                    </ul>
                 </aside>
+
+                {/* sidebar left */}
+                <aside className={styles.nav}>
+                    <ul>
+                        <li>
+                            <Link to="/">
+                                <i className={icons.home}></i>
+                                <span>home</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/">
+                                <i className={icons.explore}></i>
+                                <span>explore</span>
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <Link to="/">
+                                <i className={icons.profile}></i>
+                                <span>profile</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/">
+                                <i className={icons.messages}></i>
+                                <span>messages</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/">
+                                <i className={icons.notifications}></i>
+                                <span>notifications</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/">
+                                <i className={icons.settings}></i>
+                                <span>settings</span>
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <Link to="/">
+                                <i className={icons.about}></i>
+                                <span>about</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/">
+                                <i className={icons.contact}></i>
+                                <span>contact</span>
+                            </Link>
+                        </li>
+                    </ul>
+                    <div>
+                        <button>+ create post</button>
+                    </div>
+                </aside>
+
+                {/* center section */}
                 <section className={styles.feed}>
                     <div>
                         share post
@@ -148,18 +232,269 @@ function Home() {
                         loading with conditional | loading more posts...
                     </div>
                 </section>
+
+                {/* sidebar right */}
                 <aside className={styles.discover}>
-                    <div>
-                        tending hashtags
+                    <div className={styles.trendtags}>
+                        <div className={styles.trendtags_top}>
+                            <h3>trending hashtags</h3>
+                            <Link to="/explore">see all</Link>
+                        </div>
+                        <div className={styles.trendtags_container}>
+                            <div className={styles.hashtagcard}>
+                                <div>
+                                    <img src="" alt="photo" />
+                                </div>
+                                <div>
+                                    <strong>#category</strong>
+                                    <span>1.2M posts</span>
+                                </div>
+                            </div>
+                            <div className={styles.hashtagcard}>
+                                <div>
+                                    <img src="" alt="photo" />
+                                </div>
+                                <div>
+                                    <strong>#category</strong>
+                                    <span>1.2M posts</span>
+                                </div>
+                            </div>
+                            <div className={styles.hashtagcard}>
+                                <div>
+                                    <img src="" alt="photo" />
+                                </div>
+                                <div>
+                                    <strong>#category</strong>
+                                    <span>1.2M posts</span>
+                                </div>
+                            </div>
+                            <div className={styles.hashtagcard}>
+                                <div>
+                                    <img src="" alt="photo" />
+                                </div>
+                                <div>
+                                    <strong>#category</strong>
+                                    <span>1.2M posts</span>
+                                </div>
+                            </div>
+                            <div className={styles.hashtagcard}>
+                                <div>
+                                    <img src="" alt="photo" />
+                                </div>
+                                <div>
+                                    <strong>#category</strong>
+                                    <span>1.2M posts</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        people
+                    <div className={styles.people}>
+                        <div className={styles.trendtags_top}>
+                            <h3>people you may know hashtags</h3>
+                            <Link to="/explore">see all</Link>
+                        </div>
+                        <div className={styles.people_container}>
+                            <div className={styles.peoplecard}>
+                                <div>
+                                    <img src="" alt="photo" />
+                                </div>
+                                <div>
+                                    <h5>firstname lastname</h5>
+                                    <span>Baku, Azerbaijan</span>
+                                </div>
+                                <div>
+                                    <button>
+                                        follow
+                                    </button>
+                                    <button>
+                                        <i className={icons.xMark}></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className={styles.peoplecard}>
+                                <div>
+                                    <img src="" alt="photo" />
+                                </div>
+                                <div>
+                                    <h5>firstname lastname</h5>
+                                    <span>Baku, Azerbaijan</span>
+                                </div>
+                                <div>
+                                    <button>
+                                        follow
+                                    </button>
+                                    <button>
+                                        <i className={icons.xMark}></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className={styles.peoplecard}>
+                                <div>
+                                    <img src="" alt="photo" />
+                                </div>
+                                <div>
+                                    <h5>firstname lastname</h5>
+                                    <span>Baku, Azerbaijan</span>
+                                </div>
+                                <div>
+                                    <button>
+                                        follow
+                                    </button>
+                                    <button>
+                                        <i className={icons.xMark}></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className={styles.peoplecard}>
+                                <div>
+                                    <img src="" alt="photo" />
+                                </div>
+                                <div>
+                                    <h5>firstname lastname</h5>
+                                    <span>Baku, Azerbaijan</span>
+                                </div>
+                                <div>
+                                    <button>
+                                        follow
+                                    </button>
+                                    <button>
+                                        <i className={icons.xMark}></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div className={styles.peoplecard}>
+                                <div>
+                                    <img src="" alt="photo" />
+                                </div>
+                                <div>
+                                    <h5>firstname lastname</h5>
+                                    <span>Baku, Azerbaijan</span>
+                                </div>
+                                <div>
+                                    <button>
+                                        follow
+                                    </button>
+                                    <button>
+                                        <i className={icons.xMark}></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </aside>
             </main>
 
             <footer className={styles.footer}>
-                Footer
+                <div className={styles.top}>
+                    <div className={styles.info}>
+                        <div>
+                            <img src={Logo} alt="logo" />
+                        </div>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eveniet cum dolor?
+                        </p>
+                        <div>
+                            <button>
+                                <i className={icons.youtube}></i>
+                            </button>
+                            <button>
+                                <i className={icons.linkedin}></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div className={styles.nav}>
+                        <div className={styles.group}>
+                            <div className={styles.column}>
+                                <h4>platform</h4>
+                                <ul>
+                                    <li>
+                                        <Link to="/">home</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">explore</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">messages</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">notifications</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">profile</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className={styles.column}>
+                                <h4>company</h4>
+                                <ul>
+                                    <li>
+                                        <Link to="/">about</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">contact</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">blog</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">careers</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">press</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={styles.group}>
+                            <div className={styles.column}>
+                                <h4>support</h4>
+                                <ul>
+                                    <li>
+                                        <Link to="/">help center</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">terms of service</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">privacy policy</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">community guidelines</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/">cookies policy</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className={styles.column}>
+                                <h4>contact us</h4>
+                                <ul>
+                                    <li>
+                                        <i className={icons.envelopeAt}></i>
+                                        <span>hello@socialmediaapp.com</span>
+                                    </li>
+                                    <li>
+                                        <i className={icons.telephone}></i>
+                                        <span>+994-01-234-56-78</span>
+                                    </li>
+                                    <li>
+                                        <i className={icons.map}></i>
+                                        <span>987 Untitled Street, Baku</span>
+                                    </li>
+                                    <li>
+                                        <i className={icons.postage}></i>
+                                        <span>AZ 1234, Azerbaijan</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.bottom}>
+                    <p>
+                        © 2026 SocialMediaApp. All rights reserved.
+                    </p>
+                </div>
             </footer>
         </>
     );
--- frontend/src/utilities/constants/icons.bsClassNames.js
+++ frontend/src/utilities/constants/icons.bsClassNames.js
@@ -2,11 +2,38 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 const bootstrapIcons = {
     chevronDown: "bi bi-chevron-down",
+    xMark: "bi bi-x",
     
     sun: "bi bi-sun",
     sunFill: "bi bi-sun-fill",
     moon: "bi bi-moon",
     moonFill: "bi bi-moon-fill",
+
+    youtube: "bi bi-youtube",
+    linkedin: "bi bi-linkedin",
+
+    envelopeAt: "bi bi-envelope-at",
+    telephone: "bi bi-telephone",
+    map: "bi bi-geo-alt",
+    postage: "bi bi-postage",
+
+    home: "bi bi-house",
+    explore: "bi bi-compass",
+    profile: "bi bi-person",
+    messages: "bi bi-envelope",
+    notifications: "bi bi-bell",
+    settings: "bi bi-gear",
+    about: "bi bi-info-circle",
+    contact: "bi bi-headset",
+
+    dashboard: "bi bi-speedometer2",
+    notifications: "bi bi-app-indicator",
+    users: "bi bi-people",
+    posts: "bi bi-broadcast",
+    comments: "bi bi-signal",
+
+    like: "bi bi-hand-thumb-up",
+    dislike: "bi bi-hand-thumb-down",
 };
 
 export default bootstrapIcons;
